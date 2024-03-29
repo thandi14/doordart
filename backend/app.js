a534f816266bffd16105ef27ab7a32b5f911cbf6
thandi mpofu <thandimpofu2003@gmail.com>
2024-03-29 09:02:06 -0500

    error handling

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -18,28 +19,64 @@ app.use(express.json());
 
 
 if (!isProduction) {
-    // enable cors only in development
-    app.use(cors());
+// enable cors only in development
+app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+})
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+csurf({
+    cookie: {
+    secure: isProduction,
+    sameSite: isProduction && "Lax",
+    httpOnly: true
+    }
+})
+);
+
+app.use(routes)
+
+app.use((_req, _res, next) => {
+const err = new Error("The requested resource couldn't be found.");
+err.title = "Resource Not Found";
+err.errors = { message: "The requested resource couldn't be found." };
+err.status = 404;
+next(err);
+});
+
+// ...
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
   }
+  next(err);
+});
 
-  // helmet helps set a variety of headers to better secure your app
-  app.use(
-    helmet.crossOriginResourcePolicy({
-      policy: "cross-origin"
-    })
-  );
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+});
 
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
 
-  app.use(routes)
-
-  module.exports = app;
+module.exports = app;
